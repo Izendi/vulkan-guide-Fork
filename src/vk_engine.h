b1e94ccf2b1a96aa4472f1cc9d6878fe12cb147b
Izendi <cjwinters1404@gmail.com>
2024-08-01 22:09:45 +0800

    flashing blue screen

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -5,8 +5,29 @@
 
 #include <vk_types.h>
 
+constexpr unsigned int FRAME_OVERLAP = 2;
+
+struct FrameData
+{
+	VkCommandPool _commandPool;
+	VkCommandBuffer _mainCommandBuffer;
+	
+	VkSemaphore _swapchainSemaphore, _renderSemaphore;
+	VkFence _renderFence;
+};
+
 class VulkanEngine {
 public:
+
+	FrameData _frames[FRAME_OVERLAP];
+
+	FrameData& get_current_frame()
+	{
+		return _frames[_frameNumber % FRAME_OVERLAP];
+	}
+
+	VkQueue _graphicsQueue;
+	uint32_t _graphicsQueueFamily;
 
 	bool _isInitialized{ false };
 	int _frameNumber {0};
@@ -14,6 +35,12 @@ public:
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
 	struct SDL_Window* _window{ nullptr };
+
+	VkInstance _instance;// Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface;// Vulkan window surface
 
 	static VulkanEngine& Get();
 
@@ -29,9 +56,21 @@ public:
 	//run main loop
 	void run();
 
+	//Public Swapchain related functions:
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+
 private:
 	void init_vulkan();
 	void init_swapchain();
 	void init_commands();
 	void init_sync_structures();
+
+	//Private Swapchain related functions:
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
 };
